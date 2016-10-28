3f152ec38745dde4d8c03a0de38bf5b31b8c4f0a
Kay Lovelace <kay@herostrat.us>
2016-10-28 09:15:38 +0100

    Break message ordering so we can TDD it
    😎

--- app/assets/javascripts/chat_service.js
+++ app/assets/javascripts/chat_service.js
@@ -4,7 +4,7 @@ function ChatServiceFactory() {
   var messageList = [];
 
   function submitMessage(message) {
-    messageList.push(message);
+    messageList.unshift(message);
   }
 
   function getMessageList() {
