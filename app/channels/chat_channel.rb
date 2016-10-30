8e5b1a1d1ea82e25237f4bf00b1a6a723c03fbfd
Kay Lovelace <kay@herostrat.us>
2016-10-30 09:53:02 +0000

    Resolve channel instance variable weirdness
    Instance variables get set up in `#subscribed`!
    See http://api.rubyonrails.org/classes/ActionCable/Channel/Base.html

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,16 +1,13 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ChatChannel < ApplicationCable::Channel
   def subscribed
+    @message_store = MessageStore.new
     stream_from "the_room"
     transmit({eventType: "syncMessages", messages: Message.order(created_at: :asc)})
   end
 
   def receive(data)
-    # Channels have some strange initialization / forking? stuff going on which
-    # hampers our attempt to make this an instance variable, so we'll initialize
-    # it each time for now
-    message_store = MessageStore.new
-    message = message_store.receive(data)
+    message = @message_store.receive(data)
     ActionCable.server.broadcast("the_room", {eventType: "newMessage", message: message})
   end
 end
