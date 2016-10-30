9d50248ee12b2093ad10d2b4e11e1de338fa5a51
Kay Lovelace <kay@herostrat.us>
2016-10-30 22:20:30 +0000

    Switch to Postgres for Heroku's pleasure

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161030175004) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string   "username"
     t.text     "body"
